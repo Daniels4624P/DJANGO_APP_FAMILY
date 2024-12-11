5a59ea60d52e679c5c135aabe399c75185f6e3e3
Daniel <96382302+Daniels4624P@users.noreply.github.com>
2024-12-11 15:37:27 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
+python manage.py makemigrations
 python manage.py migrate
 python manage.py populate_tasks
 python manage.py populate_proyetcs
