6cd6f38ce3512ec9557d5fbe8b42f6d53fc9b08a
Daniel <96382302+Daniels4624P@users.noreply.github.com>
2024-12-11 15:43:04 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py populate_tasks
 python manage.py populate_proyetcs
+curl -fsSL https://fnm.vercel.app/install | bash
+source ~/.bashrc
+fnm use --install-if-missing 22
 python manage.py tailwind build
 celery -A Tareas_Casa worker --loglevel=info
 celery -A Tareas_Casa beat --loglevel=info
