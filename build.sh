3f5e6b2987b0d7b2443f14d6b10991a12b680a46
Daniel <96382302+Daniels4624P@users.noreply.github.com>
2024-12-11 16:00:49 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,11 @@ set -o errexit
 
 pip install -r requirements.txt
 
+cd theme
+npm install
+npm run build
+cd ..
+
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
