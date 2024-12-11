dc58cc918aee63912be4263cbc711dac5a54c294
Daniel <96382302+Daniels4624P@users.noreply.github.com>
2024-12-11 17:38:41 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,3 +17,19 @@ python manage.py populate_tasks
 python manage.py populate_proyetcs
 python manage.py tailwind build
 python manage.py migrate django_celery_beat
+echo "Creando superusuario..."
+python manage.py shell <<EOF
+from django.contrib.auth.models import User
+
+# Datos del superusuario
+username = "admin"
+email = "admin@example.com"
+password = "admin123"
+
+# Crear superusuario solo si no existe
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f"Superusuario creado: {username}")
+else:
+    print(f"Superusuario ya existe: {username}")
+EOF
