9c641b584187b40c6ae53d4766b82195f624d9e3
Daniel <96382302+Daniels4624P@users.noreply.github.com>
2024-12-17 11:08:38 -0500

    Update settings.py

--- Tareas_Casa/settings.py
+++ Tareas_Casa/settings.py
@@ -157,3 +157,9 @@ INTERNAL_IPS = [
 ]
 
 NPM_BIN_PATH = "/home/render/envwrappers/npm"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "taskmanager61@gmail.com"
+EMAIL_HOST_PASSWORD = "ftlucrdbiaanpeuz"
