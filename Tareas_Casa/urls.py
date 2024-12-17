a6d1decbdc0a5255cca3021034963761db9a475c
Daniel <96382302+Daniels4624P@users.noreply.github.com>
2024-12-17 11:08:10 -0500

    Update urls.py

--- Tareas_Casa/urls.py
+++ Tareas_Casa/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from Tareas.views import SignUp, Tareas, SignIn, Home, Logout, UpPoints, DecrementPoints, Anadir_Tarea, Proyectos_Function, Eliminar_Tarea, tabla_posiciones, Anadir_Proyecto, Completar_Proyectos, Descompletar_Proyectos, Crear_Observacion, Editar_Observacion, UpPointsAreas, Tabla_Tareas_Persona
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -39,4 +40,8 @@ urlpatterns = [
     path('subir_puntos_areas/<int:task_id>', UpPointsAreas, name='UpPointsAreas'),
     path("__reload__/", include("django_browser_reload.urls")),
     path("Tareas_Persona/", Tabla_Tareas_Persona, name='TareasPorPersona'),
+    path("password_reset/", auth_views.PasswordResetView.as_view(template_name="password_reset.html"), name='password_reset'),
+    path("password_reset_done/", auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"), name='password_reset_done'),
+    path("password_reset_confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"), name='password_reset_confirm'),
+    path("password_reset_complete/", auth_views.PasswordResetDoneView.as_view(template_name="password_reset_complete.html"), name='password_reset_complete'),
 ]
