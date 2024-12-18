2c44e1fd0b94f73e06a67cef933866bb66236390
Daniel <96382302+Daniels4624P@users.noreply.github.com>
2024-12-18 11:46:30 -0500

    Update models.py

--- Tareas/models.py
+++ Tareas/models.py
@@ -17,13 +17,13 @@ class Tasks(models.Model):
         return self.nombre
 
 class UserTask(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     task = models.ForeignKey(Tasks, on_delete=models.CASCADE)
     completed = models.BooleanField(default=False)
     completed_at = models.DateTimeField(blank=True, null=True, auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username} - {self.task.nombre} - {self.completed_at.strftime('%d-%m-%Y')}"
+        return f"{self.user.username} - {self.task.nombre} - {self.completed_at.strftime("%d-%m-%Y")}"
 
 class Proyectos(models.Model):
     proyecto = models.CharField(max_length=100)
@@ -33,3 +33,11 @@ class Proyectos(models.Model):
 
     def __str__(self):
         return f"Proyecto: {self.proyecto} - Completado: {self.completado}"
+    
+class TaskHistory(models.Model):
+    tarea = models.ForeignKey(Tasks, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    completed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.tarea.nombre} - {self.completed_at.strftime("%d-%m-%Y")}"
