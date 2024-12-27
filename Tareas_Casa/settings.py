f03e57544252c7b5071c9cca7a9d867f54d4a6f3
Daniel <96382302+Daniels4624P@users.noreply.github.com>
2024-12-27 17:41:55 -0500

    Update settings.py

--- Tareas_Casa/settings.py
+++ Tareas_Casa/settings.py
@@ -146,7 +146,7 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_BEAT_SCHEDULE = {
     'mi-tarea-periodica': {
         'task': 'Tareas.tasks.descompletar_tareas',
-        'schedule': crontab(hour=0, minute=0),  # Cada 30 segundos
+        'schedule': crontab(hour=17, minute=50),  # Cada 30 segundos
     },
 }
 
