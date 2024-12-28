ecf7ac4c99ddcbe466602ed3e639434a265ac038
Daniel <96382302+Daniels4624P@users.noreply.github.com>
2024-12-27 22:36:29 -0500

    Update celery.py

--- Tareas_Casa/celery.py
+++ Tareas_Casa/celery.py
@@ -6,7 +6,9 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tareas_Casa.settings')
 
 app = Celery('Tareas_Casa')
-
+app.conf.update(
+    broker_connection_timeout=30,  # Tiempo de espera en segundos
+)
 # Configura Celery para usar las configuraciones de Django
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
